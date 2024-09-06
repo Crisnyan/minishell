4af6ffc248c7445234df03cc893b403b3e30133b
Cristian <cristianangelogaucho@gmail.com>
2024-09-06 17:20:42 +0200

    replaced placeholder function if names

--- src/expansor/expand_string.c
+++ src/expansor/expand_string.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expand_sting.c                                     :+:      :+:    :+:   */
+/*   expand_string.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cristian <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 01:57:00 by cristian          #+#    #+#             */
-/*   Updated: 2024/09/03 01:57:12 by cristian         ###   ########.fr       */
+/*   Updated: 2024/09/06 17:17:16 by cristian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static void	if1(t_token *tok, t_dict *m_env, char **str, int *n)
+static void	case_expand_append(t_token *tok, t_dict *m_env, char **str, int *n)
 {
 	if (tok->data[n[I]] == '$' && tok->data[n[I] + 1]
 		&& (!is_quote(tok->data[n[I] + 1]) && !is_space(tok->data[n[I] + 1])))
@@ -38,7 +38,7 @@ static void	if1(t_token *tok, t_dict *m_env, char **str, int *n)
 	}
 }
 
-static void	if2(t_token *tok, char **str, int *nums)
+static void	case_expand_null(t_token *tok, char **str, int *nums)
 {
 	str[AUX] = ft_substr(tok->data, nums[J], nums[I] - nums[J]);
 	str[RES] = ft_strappend(&str[RES], str[AUX]);
@@ -47,14 +47,14 @@ static void	if2(t_token *tok, char **str, int *nums)
 	str[RES] = ft_strappend(&str[RES], NULL);
 }
 
-static void	if3(t_token *tok, char **str, int *nums)
+static void	case_append(t_token *tok, char **str, int *nums)
 {
 	str[AUX] = ft_substr(tok->data, nums[J], nums[I]);
 	str[RES] = ft_strappend(&str[RES], str[AUX]);
 	free(str[AUX]);
 }
 
-static void	if4(t_token *tok, char **str)
+static void	case_none(t_token *tok, char **str)
 {
 	free(tok->data);
 	tok->data = ft_strdup(str[RES]);
@@ -74,16 +74,16 @@ void	expand_string(t_token *tok, t_dict *m_env)
 		if (tok->data[nums[I]] == '$' && tok->data[nums[I] + 1]
 			&& (!is_quote(tok->data[nums[I] + 1])
 				&& !is_space(tok->data[nums[I] + 1])))
-			if1(tok, m_env, str, nums);
+			case_expand_append(tok, m_env, str, nums);
 		if (tok->next && (tok->next->flags == FOLLOW_QUOTE
 				|| tok->next->flags == FOLLOW_DQUOTE)
 			&& (tok->flags == STRING || tok->flags == FOLLOW_STRING)
 			&& tok->data[nums[I]] == '$' && !tok->data[nums[I] + 1])
-			if2(tok, str, nums);
+			case_expand_null(tok, str, nums);
 		nums[I]++;
 	}
 	if (tok->data[nums[J]])
-		if3(tok, str, nums);
+		case_append(tok, str, nums);
 	if (str[RES])
-		if4(tok, str);
+		case_none(tok, str);
 }
